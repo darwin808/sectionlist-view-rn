422ff4691dc36709b1ee368f5a15a71c6c2be64a
darwin apolinario <darwin.apolinario12>
2023-06-24 12:18:01 +0800

    feat:  update

--- src/Screen/constant.tsx
+++ src/Screen/constant.tsx
@@ -1,41 +1,31 @@
 import { useState } from "react";
-import {
-  FlatList,
-  Pressable,
-  SectionList,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export const DummyComp = ({
-  onPress,
-  ht = 300,
-  bgColor = "red",
-  txt = "",
-}: any) => {
+export const DummyComp = ({ ht = 300, bgColor = "red", txt = "" }: any) => {
   const [toggle, settoggle] = useState(false);
   return (
-    <View style={{ width: "100%", height: ht, backgroundColor: bgColor }}>
-      <View style={{ flex: 1 }}>
-        <Text
-          style={{
-            textAlign: "center",
-            marginVertical: "auto",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingTop: 50,
-          }}
-        >
-          {txt}
-        </Text>
-        <TouchableOpacity
-          onPress={() => {
-            settoggle(!toggle);
-          }}
-        >
-          <Text>Click me</Text>
-        </TouchableOpacity>
+    <View>
+      <View style={{ width: "100%", height: ht, backgroundColor: bgColor }}>
+        <View style={{ flex: 1 }}>
+          <Text
+            style={{
+              textAlign: "center",
+              marginVertical: "auto",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: 50,
+            }}
+          >
+            {txt}
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              settoggle(!toggle);
+            }}
+          >
+            <Text>Click me</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       {toggle && (
         <View style={{ height: ht * 0.5, backgroundColor: "orange" }}>
